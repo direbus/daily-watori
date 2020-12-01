a39eb3ec0ad535b6118a59eb623b8fbf2b15a22b
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-12-01 21:40:32 +0700

    feat: Add migrations

--- migrations/1606830567440-seed.ts
+++ migrations/1606830567440-seed.ts
@@ -21,9 +21,24 @@ export class seed1606830567440 implements MigrationInterface {
         FOREIGN KEY (userId) REFERENCES user (id)
       );`,
     );
+
+    await queryRunner.query(
+      `CREATE INDEX tweet_hash_index
+      ON tweet USING HASH (tweet_id)`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    
+    await queryRunner.query(
+      `DROP INDEX tweet_hash_index`,
+    );
+
+    await queryRunner.query(
+      `DROP TABLE tweet`,
+    );
+
+    await queryRunner.query(
+      `DROP TABLE user`,
+    );
   }
 }
