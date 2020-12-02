db88804dcdb7cf06174b929ff6637177420ac3df
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-12-02 15:20:14 +0700

    chore: Add direct migrations script and fix migrations issues

--- migrations/1606830567440-seed.ts
+++ migrations/1606830567440-seed.ts
@@ -3,14 +3,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class seed1606830567440 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE user (
+      `CREATE TABLE "user" (
         id SERIAL PRIMARY KEY,
         name VARCHAR(128) NOT NULL
       );`,
     );
 
     await queryRunner.query(
-      `CREATE TABLE tweet (
+      `CREATE TABLE "tweet" (
         id SERIAL PRIMARY KEY,
         url TEXT NOT NULL,
         tweet_id VARCHAR(64) UNIQUE NOT NULL,
@@ -18,27 +18,27 @@ export class seed1606830567440 implements MigrationInterface {
         approved_at TIMESTAMP,
         has_retweeted BOOL NOT NULL,
         userId INT NOT NULL,
-        FOREIGN KEY (userId) REFERENCES user (id)
+        FOREIGN KEY (userId) REFERENCES "user" (id)
       );`,
     );
 
     await queryRunner.query(
-      `CREATE INDEX tweet_hash_index
-      ON tweet USING HASH (tweet_id)`,
+      `CREATE INDEX "tweet_hash_index"
+      ON "tweet" USING HASH (tweet_id)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DROP INDEX tweet_hash_index`,
+      `DROP INDEX "tweet_hash_index"`,
     );
 
     await queryRunner.query(
-      `DROP TABLE tweet`,
+      `DROP TABLE "tweet"`,
     );
 
     await queryRunner.query(
-      `DROP TABLE user`,
+      `DROP TABLE "user"`,
     );
   }
 }
