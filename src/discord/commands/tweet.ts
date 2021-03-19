78a4700e52f09f8e39032fda4a4de9683a4530b0
Chris <christianto.g.14@gmail.com>
2021-03-19 10:01:47 +0700

    feat: Add more information to tweet command.
    Ref #23

--- src/discord/commands/tweet.ts
+++ src/discord/commands/tweet.ts
@@ -6,15 +6,21 @@ import { prefix } from './../../../bot.config.json';
 export default {
   command: 'tweet',
   description: 'Tweets things, instantly.',
-  params: [],
+  params: [
+    {
+      name: 'text',
+      description: 'Tweet message/status.',
+    },
+  ],
   example: `\`${prefix}tweet New episode on sitename, looking forward to watch it!\``,
   execute: async (
     message: Message,
-    { twitterRepository }: Context,
+    args: string[],
+    { twitterRepository: twitterService }: Context,
   ): Promise<Message> => {
     const { channel } = message;
     try {
-      await twitterRepository.sendTweet(message.content.substr(prefix.length));
+      await twitterService.sendTweet(message.content.substr(prefix.length + 'tweet'.length));
     } catch (e) {
       return await channel.send('Error while sending the tweet.', errorToEmbed(e));
     }
