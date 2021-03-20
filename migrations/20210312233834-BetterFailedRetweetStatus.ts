9ece4cb57cc61a26086d81288f2ca7ea82fec704
Chris <christianto.g.14@gmail.com>
2021-03-20 16:36:12 +0700

    chore: Reformat migration pack

--- migrations/20210312233834-BetterFailedRetweetStatus.ts
+++ migrations/20210312233834-BetterFailedRetweetStatus.ts
@@ -3,11 +3,27 @@ import { TweetEntity } from '../src/entity/tweet';
 
 module.exports = {
   async up(db: Db): Promise<void> {
-    await db.collection<TweetEntity>('tweet').updateMany({}, { $set: { failedAt: undefined, retweetedAt: undefined } });
-    await db.collection<TweetEntity>('tweet').updateMany({ hasRetweeted: true }, { $set: { retweetedAt: (new Date()) } });
+    await db.collection<TweetEntity>('tweet').updateMany({}, {
+      $set: {
+        failedAt: undefined,
+        retweetedAt: undefined
+      }
+    });
+    await db.collection<TweetEntity>('tweet').updateMany({
+      hasRetweeted: true
+    }, {
+      $set: {
+        retweetedAt: (new Date())
+      }
+    });
   },
 
   async down(db: Db): Promise<void> {
-    await db.collection<TweetEntity>('tweet').updateMany({}, { $unset: { failedAt: 1, retweetedAt: 1 } });
+    await db.collection<TweetEntity>('tweet').updateMany({}, {
+      $unset: {
+        failedAt: 1,
+        retweetedAt: 1
+      },
+    });
   }
 };
