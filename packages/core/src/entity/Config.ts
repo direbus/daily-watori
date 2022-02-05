a84cf1fd0dce0b4ed1bf2c9b4692abd4d497ffc8
Chris "chez14" Qiang <chris@christianto.net>
2022-02-06 01:59:31 +0700

    chore(core): add config entity

---
+++ packages/core/src/entity/Config.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity({ name: "config" })
+export class Config {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  value: string;
+}
